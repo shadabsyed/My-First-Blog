d3a4d7fc495322a64bc37aba412f70252eb61732
Shadab Syed <shadabsyed488@gmail.com>
2023-06-26 13:05:29 +0500

    vite app set up

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SinglePost from "./components/SinglePost";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
