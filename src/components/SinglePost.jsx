9c1b5e9d15b9a66e41d5c5a8d0cc3286a35711cd
Shadab Syed <shadabsyed488@gmail.com>
2023-06-26 18:44:18 +0500

    step of fetching category

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { formatDate } from "../modules/formatDate";
-const SinglePost = () => {
+
+function SinglePost() {
   const { slug } = useParams();
 
   const {
@@ -23,15 +23,42 @@ const SinglePost = () => {
 
   const data = postData[0];
 
+  if (postData.length === 0) {
+    return "Sorry, The requested post was not found.";
+  }
+
+  const categoryId = data.categories[0];
+  const {
+    isLoading: categoryLoading,
+    error: categoryError,
+    data: categoryData,
+  } = useQuery({
+    queryKey: ["category", categoryId],
+    queryFn: () =>
+      fetch(`http://site1.local/wp-json/wp/v2/categories/${categoryId}`).then(
+        (res) => res.json()
+      ),
+  });
+
+  if (categoryLoading) return "Loading Category......";
+
+  if (categoryError) return "Oops! there is error fetching category";
+
+  const categoryName = categoryData;
+
   return (
     <div className="card">
       <div className="card-body">
         <h2 className="card-title">{data.title.rendered}</h2>
+        {categoryName && <p className="card-text">{categoryName.name}</p>}
         <p className="card-text">{formatDate(data.date)}</p>
-        <div dangerouslySetInnerHTML={{ __html: data.content.rendered }}></div>
+        <div
+          dangerouslySetInnerHTML={{ __html: data.content.rendered }}
+          className="card-text"
+        ></div>
       </div>
     </div>
   );
-};
+}
 
 export default SinglePost;
